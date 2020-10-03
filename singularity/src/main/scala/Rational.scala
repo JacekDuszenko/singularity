8e86b55cd27668e8e8068aa41f89020910011cb7
Jacek Duszenko <jacek.duszenko@scanye.pl>
2020-10-03 22:13:29 +0200

    arithmetic operations

--- singularity/src/main/scala/Rational.scala
+++ singularity/src/main/scala/Rational.scala
@@ -1,10 +1,65 @@
+import scala.annotation.tailrec
+
 object Rational {
   def apply(nominator: Int, denominator: Int): Rational = new Rational(nominator, denominator)
+
+  def apply(nominator: Int): Rational = new Rational(nominator)
 }
 
-class Rational(nominator: Int, denominator: Int) {
-  require(denominator != 0, "denominator must be nonzero")
-  print (this.toString)
+class Rational(n: Int, d: Int) {
+  require(d != 0, "denominator must be nonzero")
+  private val g        = gcd(n.abs, d.abs)
+  val numerator: Int   = n / g
+  val denominator: Int = d / g
 
-  override def toString: String = s"($nominator / $denominator)"
+  def this(num: Int) = this(num, 1)
+
+  def +(that: Rational): Rational = {
+    Rational(
+      numerator * that.denominator + that.numerator * denominator,
+      that.denominator * denominator
+    )
+  }
+
+  def +(i: Int): Rational = {
+    Rational(numerator + i * denominator, denominator)
+  }
+
+  def -(that: Rational): Rational = {
+    Rational(
+      numerator * that.denominator - that.numerator * denominator,
+      denominator * that.denominator
+    )
+  }
+
+  def -(i: Int): Rational = {
+    Rational(numerator - i * denominator, denominator)
+  }
+
+  def *(that: Rational): Rational = {
+    Rational(numerator * that.numerator, denominator + that.denominator)
+  }
+
+  def *(i: Int): Rational = {
+    Rational(numerator * i, denominator)
+  }
+
+  def /(that: Rational): Rational = {
+    Rational(numerator * that.denominator, denominator * that.numerator)
+  }
+
+  def /(i: Int): Rational = {
+    Rational(numerator, denominator * i)
+  }
+
+  def lt(that: Rational): Boolean = {
+    this.numerator * that.denominator < this.denominator * that.numerator
+  }
+
+  def max(that: Rational): Rational = if (lt(that)) that else this
+
+  @tailrec
+  private final def gcd(a: Int, b: Int): Int = if (a % b == 0) b else gcd(b, a % b)
+
+  override def toString: String = s"($numerator / $denominator)"
 }
---
+++ singularity/src/test/scala/typeclasses/RationalTest.scala
@@ -0,0 +1,28 @@
+package typeclasses
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class RationalTest extends AnyFlatSpec {
+
+  it should "throw IllegalArgumentException when rational initiated with 0 as denominator" in {
+    assertThrows[IllegalArgumentException] {
+      Rational(2, 0)
+    }
+  }
+  it should "add two rationals correctly" in {
+    val expectedRat = Rational(5, 6)
+
+    val fst = Rational(1, 2)
+    val snd = Rational(1, 3)
+
+    fst + snd shouldBe expectedRat
+  }
+
+  it should "reduce rational to the simples form" in {
+    val expectedRat = Rational(1, 2)
+
+    val expandedRat = Rational(100, 200)
+
+    expandedRat shouldBe expectedRat
+  }
+}
