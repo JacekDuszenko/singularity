f9c0e5e86db20599ccc9945e6721d7f9ab74c197
Jacek Duszenko <jacek.duszenko@scanye.pl>
2020-10-11 20:53:28 +0200

    start interpreter

--- singularity/src/main/scala/Main.scala
+++ singularity/src/main/scala/Main.scala
@@ -6,10 +6,16 @@ object Main {
     a + b + c + d
   }
 
+  def s(a: Int)(b: Int): Int = {
+    a + b
+  }
+
   def main(params: Array[String]): Unit = {
 
-    val a = Rational(2)
-    val c = 3 * a
+    val curriedSum = s(5) _
+    val res        = curriedSum(500)
+    val a          = Rational(2)
+    val c          = 3 * a
     Seq(1, 2, 3).exists(_ % 2 == 1)
     val f  = sum _
     val f2 = sum(_, _, _, _)
---
+++ singularity/src/main/scala/interpreter/SingularityInterpreter.scala
@@ -0,0 +1,9 @@
+package interpreter
+
+/**
+  * Main entrypoint to the language interpreter
+  */
+class SingularityInterpreter {
+
+  def interpret(code: String): Unit = {}
+}
