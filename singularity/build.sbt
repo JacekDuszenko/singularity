6adc100eebda0a1703cebe97704919c2aab8586f
Jacek Duszenko <jacek.duszenko@scanye.pl>
2020-11-08 06:55:23 +0100

    lexer debugging

--- singularity/build.sbt
+++ singularity/build.sbt
@@ -4,16 +4,16 @@ scalaVersion := "2.13.1"
 
 name := "singularity"
 organization := "com.jacekduszenko"
-version := "0.0.1"
+version := "0.0.2"
 
 // regular deps
-libraryDependencies += "org.typelevel"          %% "cats-core"                % "2.0.0"
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
-libraryDependencies += "org.typelevel"          %% "cats-core"                % "2.0.0"
+libraryDependencies += "org.typelevel"          %% "cats-core"                     % "2.0.0"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators_2.13" % "1.1.2"
+libraryDependencies += "org.typelevel"          %% "cats-core"                     % "2.0.0"
 
 //test deps
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "3.3.0-SNAP2"  % "test",
-  "org.scalacheck" %% "scalacheck" % "1.14.1" % "test",
+  "org.scalatest"     %% "scalatest"                % "3.3.0-SNAP2" % "test",
+  "org.scalacheck"    %% "scalacheck"               % "1.14.1"      % "test",
   "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2" % "test"
 )
--- singularity/src/main/scala/lexer/SchemeLexer.scala
+++ singularity/src/main/scala/lexer/SchemeLexer.scala
@@ -1,96 +1,107 @@
 package lexer
 
-import lexer.ConstantTokenLexer.{bool, float, int, regex, str}
-import lexer.SchemeLexer.{inParens, lp, rp}
+import lexer.SchemeLexer.{AnyValToken, program}
 
-import scala.util.parsing.combinator.{Parsers, RegexParsers}
+import scala.util.parsing.combinator.RegexParsers
 
-object SchemeLexer extends Parsers {
-  val lp = "\\(".r ^^ identity
-  val rp = "\\)".r ^^ identity
+object SchemeLexer extends RegexParsers {
+  type AnyValToken = String //replace with real tokens soon
 
-  implicit class CombinatorOps[A](leftArg: Parser[A]) {
-    def &(rightArg: Parser[A]) = leftArg ~ rightArg
+  val program             = m(commandOrDefinition)(`*`)
+
+  private val commandOrDefinition = command | definition
+  private val command             = expression
+  private val definition          = p("define".r ~ variable ~ expression ^^ {_.toString})
+
+  private val expression       = variable | literal | procedureCall | lambdaExpression | conditional | assignment | derivedExpression
+  private val literal          = selfEvaluating
+  private val selfEvaluating   = boolean | number | character | string
+  private val procedureCall    = p(operator ~ m(operand)(`*`) ^^ {_.toString})
+  private val operator         = expression
+  private val operand          = expression
+  private val lambdaExpression = p("lambda".r ~ formals ~ body ^^{_.toString})
+  private val formals          = p(m(variable)(`*`)) | variable | p(m(variable)(`+`) | "\\.".r | variable)
+  private val body             = definition.* ~ sequence
+  private val sequence         = command.* ~ sequence
+  private val conditional      = p("if".r ~ test ~ consequent ~ alternate ^^ {_.toString})
+  private val test             = expression
+  private val consequent       = expression
+  private val alternate        = expression
+  private val assignment       = p("set!".r ~ variable ~ expression ^^ {_.toString})
+  private val derivedExpression =
+    (p("and".r ~ m(test)(`*`) ^^ {_.toString}) | p("or".r ~ m(test)(`*`) ^^ {_.toString})
+    | p("let".r ~ p(m(bindingSpec)(`*`)) ~ body ^^ {_.toString})
+    | p("letrec".r ~ p(m(bindingSpec)(`*`)) ~ body ^^ {_.toString}))
+
+
+  private val bindingSpec: Parser[String] = p(variable ~ expression ^^ {_.toString})
+
+  private val keyword     = identifier
+  private val simpleDatum = boolean | number | character | string | symbol
+  private val symbol      = identifier
+
+  private val compoundDatum = list
+  private val list          = p(m(datum)(`*`)) | (p(m(datum)(`+`) ~ "\\.".r ~ datum ^^ {_.toString}))
+  private val datum         = simpleDatum | compoundDatum
+
+  private val singleQuote     = """"""".r
+  private val token           = identifier | boolean | number | character | string | lp | rp | ("#".r ~ lp) | "'".r | "`".r | ",".r | ",@".r | "\\.".r
+  private val whitespace      = " ".r | "\n".r
+  private val delimiter       = whitespace | "\\(".r | "\\)".r | ";".r | singleQuote
+  private val comment         = ";.*^".r
+  private val atmosphere      = whitespace | comment
+  private val intertokenSpace = atmosphere.*
+  private val identifier      = initial ~ m(subsequent)(`*`) ^^ { _.toString }
+  private val variable        = identifier
+  private val specialInitial = "!".r | "\\$".r | "%".r | "~".r | "\\*".r | "/".r | ":".r | "<".r | "=".r | ">".r |
+    "\\?".r | "\\^".r | "_".r | "&".r
+
+  private val letter  = "[a-z]".r
+  private val initial = specialInitial | letter
+  private val digit   = "0".r | "1".r | "2".r | "3".r | "4".r | "5".r | "6".r | "7".r | "8".r | "9".r
+
+  private val subsequent = initial | digit | specialSubsequent
+
+  private val specialSubsequent = "\\+".r | "-".r | "\\.".r | "@".r
+  private val expressionKeyword = "quote".r | "lambda".r | "if".r | "set!".r | "begin".r | "cond".r | "and".r | "or".r | "case".r | "let".r |
+    "letrec".r | "do".r | "delay".r | "quasiquote".r
+
+  private val syntacticKeyword = "else".r | "=>".r | "define".r | "unquote".r | "unquote-splicing".r | expressionKeyword
+
+  private val boolean                            = "True".r | "False".r
+  private val character                          = "\'.\'".r
+  private val string                             = """"[^"]*"""".r
+  private val number                             = "^-?\\d+$".r | simpleFloatReg | scientificNotationFloatReg
+  private val simpleFloatReg             = "^[+-]?([0-9]*)?\\.[0-9]+$".r
+  private val scientificNotationFloatReg = "-?[\\d.]+(?:E-?\\d+)?".r
+
+  private val lp: Parser[AnyValToken] = "\\(".r ^^ identity
+  private val rp: Parser[AnyValToken] = "\\)".r ^^ identity
+
+  private def p(parser: Parser[AnyValToken]): Parser[AnyValToken] = {
+    lp ~> (parser) <~ rp
   }
 
-  def inParens(parser: Parser[String]): Parser[String] = {
-    lp & (parser) & rp
+  private sealed trait MultiOpType
+  private case object `*` extends MultiOpType
+  private case object `+` extends MultiOpType
+
+  private def m(parser: Parser[AnyValToken])(tp: MultiOpType): Parser[AnyValToken] = {
+    val p = tp match {
+      case `+` => parser.*
+      case `*` => parser.+
+    }
+    p ^^ {_.mkString}
   }
 
   def apply(): SchemeLexer = new SchemeLexer()
 }
 
-class SchemeLexer extends RegexParsers with Lexer[String] {
+class SchemeLexer extends RegexParsers with Lexer[AnyValToken] {
 
-  val program             = commandOrDefinition.*
-  val commandOrDefinition = command | definition
-  val command             = expression
-  val definition          = inParens("define".r & variable & expression)
-
-  val expression       = variable | literal | procedureCall | lambdaExpression | conditional | assignment | derivedExpression
-  val literal          = selfEvaluating
-  val selfEvaluating   = boolean | number | character | string
-  val procedureCall    = inParens(operator & operand.*)
-  val operator         = expression
-  val operand          = expression
-  val lambdaExpression = inParens("lambda".r & formals & body)
-  val formals          = inParens(variable.*) | variable | inParens(variable.+ | "\\.".r | variable)
-  val body             = definition.* & sequence
-  val sequence         = command.* & sequence
-  val conditional      = inParens("if".r & test & consequent & alternate)
-  val test             = expression
-  val consequent       = expression
-  val alternate        = expression
-  val assignment       = inParens("set!".r & variable & expression)
-  val derivedExpression = {
-    inParens("and".r & test.*)
-    |.inParens("or".r & test.*)
-    |.inParens("let".r & inParens(bindingSpec.*) & body)
-    |.inParens("letrec".r & inParens(bindingSpec.*) & body)
+  def dosth() = {
+    parse(program, "assad")
   }
 
-  val bindingSpec: Parser[String] = inParens(variable & expression)
-
-  val keyword     = identifier
-  val simpleDatum = boolean | number | character | string | symbol
-  val symbol      = identifier
-
-  val compoundDatum = list
-  val list          = inParens(datum.*) | inParens((datum.+) & "\\.".r & datum)
-  val datum         = simpleDatum | compoundDatum
-
-  val singleQuote     = """"""".r
-  val token           = identifier | boolean | number | character | string | lp | rp | ("#".r & lp) | "'".r | "`".r | ",".r | ",@".r | "\\.".r
-  val whitespace      = " ".r | "\n".r
-  val delimiter       = whitespace | "\\(".r | "\\)".r | ";".r | singleQuote
-  val comment         = ";.*^".r
-  val atmosphere      = whitespace | comment
-  val intertokenSpace = atmosphere.*
-  val identifier      = (initial & subsequent.*)
-  val variable        = identifier
-  val specialInitial = "!".r | "\\$".r | "%".r | "&".r | Predef
-    .augmentString("*")
-    .r | "/".r | ":".r | "<".r | "=".r | ">".r |
-    "\\?".r | "\\^".r | "_".r | "~".r
-
-  val letter  = "[a-z]".r
-  val initial = specialInitial | letter
-  val digit   = "0".r | "1".r | "2".r | "3".r | "4".r | "5".r | "6".r | "7".r | "8".r | "9".r
-
-  val subsequent = initial | digit | specialSubsequent
-
-  val specialSubsequent = "\\+".r | "-".r | "\\.".r | "@".r
-  val expressionKeyword = "quote".r | "lambda".r | "if".r | "set!".r | "begin".r | "cond".r | "and".r | "or".r | "case".r | "let".r |
-    "letrec".r | "do".r | "delay".r | "quasiquote".r
-
-  val syntacticKeyword = "else".r | "=>".r | "define".r | "unquote".r | "unquote-splicing".r | expressionKeyword
-
-  val boolean   = bool
-  val character = """#c""".r ~ ConstantTokenLexer.char
-  val string    = str
-  val number    = int | float
-
-  override def lex(code: String) = parse(program, code)
-
-  override type Elem = this.type
+//  override def lex(code: String) = parse
 }
